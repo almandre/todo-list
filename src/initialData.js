7fab8e7174e940595e6b71a1859132a2c52a1a58
André Almeida <andreclalmeida@gmail.com>
2021-04-11 15:21:48 -0300

    Initial commit

---
+++ src/initialData.js
@@ -0,0 +1,19 @@
+const initialData = {
+    pageData: { title: 'TODO List' },
+    todos: {
+        A: {
+            body: 'Learn React Fundamentals',
+            done: true,
+        },
+        B: {
+            body: 'Build a TODOs App',
+            done: false,
+        },
+        C: {
+            body: 'Build a Game',
+            done: false,
+        },
+    },
+};
+
+export default initialData;
